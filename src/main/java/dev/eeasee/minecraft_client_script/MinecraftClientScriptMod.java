18df6167669736f87f897ed6d9c8fcd0223d1fd4
X-Green <2271174177@qq.com>
2020-10-10 19:28:56 +0800

    init

---
+++ src/main/java/dev/eeasee/minecraft_client_script/MinecraftClientScriptMod.java
@@ -0,0 +1,9 @@
+package dev.eeasee.minecraft_client_script;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MinecraftClientScriptMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
